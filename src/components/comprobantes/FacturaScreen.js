e41bbc0919592e8a5beab61482465f301d3ff898
Boris Moreno <boris_marco_moreno@hotmail.com>
2022-07-19 20:59:29 -0500

    decimales

--- src/components/comprobantes/FacturaScreen.js
+++ src/components/comprobantes/FacturaScreen.js
@@ -39,8 +39,8 @@ const headersAdicional = [
     ''
 ]
 
-export const FacturaScreen = ({history}) => {
-    const [ formValores, handleInputChangeSelect ] = useForm({
+export const FacturaScreen = ({ history }) => {
+    const [formValores, handleInputChangeSelect] = useForm({
         formaPago: ''
     });
     const { formaPago } = formValores;
@@ -63,27 +63,27 @@ export const FacturaScreen = ({history}) => {
     const { detallesFactura, adicionalesFactura, valoresFactura, claveAcceso } = useSelector(state => state.factura);
     const { mostrarCargando } = useSelector(state => state.alerta);
     const { razonSocial, direccion, mail, telefono } = formValues;
-    const { 
-        subtotalDoce, 
-        subtotalCero, 
-        subtotalNoIva, 
-        subtotalExento, 
-        subtotalSinImpuestos, 
+    const {
+        subtotalDoce,
+        subtotalCero,
+        subtotalNoIva,
+        subtotalExento,
+        subtotalSinImpuestos,
         totalIva,
-        valorTotal 
+        valorTotal
     } = valoresFactura;
     const pad = '000000000';
 
     useEffect(() => {
         const obtenerDetalles = () => {
-            if ( detallesFactura.length > 0 ) {
+            if (detallesFactura.length > 0) {
                 setDatosDetalles(detallesFactura.map(detalle => (
                     {
                         descripcion: detalle.descripcion,
-                            cantidad: detalle.cantidad,
-                            precio: detalle.valorUnitario.toFixed(2),
-                            descuento: detalle.descuento ? detalle.descuento.toFixed(2) + ' %' : '0 %',
-                            subtotal: detalle.subtotal.toFixed(2)
+                        cantidad: detalle.cantidad,
+                        precio: detalle.valorUnitario.toFixed(2),
+                        descuento: detalle.descuento ? detalle.descuento.toFixed(2) + ' %' : '0 %',
+                        subtotal: detalle.subtotal.toFixed(2)
                     }
                 )));
             } else {
@@ -110,7 +110,7 @@ export const FacturaScreen = ({history}) => {
     }, [numeroIdentificacion, dispatch])
 
     useEffect(() => {
-        if ( clienteSeleccionado ) {
+        if (clienteSeleccionado) {
             setFormValues({
                 razonSocial: clienteSeleccionado.razonSocial,
                 direccion: clienteSeleccionado.direccion,
@@ -129,13 +129,13 @@ export const FacturaScreen = ({history}) => {
     }, [dispatch])
 
     const handleClickOutside = event => {
-        const {current: wrap} = wrapperRef;
+        const { current: wrap } = wrapperRef;
         if (wrap && !wrap.contains(event.target)) {
             setDisplay(false);
         }
     }
 
-    const handleInputChange = ({target}) => {
+    const handleInputChange = ({ target }) => {
         setFormValues({
             ...formValues,
             [target.name]: target.value
@@ -156,39 +156,39 @@ export const FacturaScreen = ({history}) => {
 
     const handleEmitirFactura = () => {
         let dd = fechaEmision.getDate();
-        let mm = fechaEmision.getMonth()+1; //January is 0!
+        let mm = fechaEmision.getMonth() + 1; //January is 0!
         let yyyy = fechaEmision.getFullYear();
-        if(dd<10){
-                dd='0'+dd
-            } 
-            if(mm<10){
-                mm='0'+mm
-            } 
+        if (dd < 10) {
+            dd = '0' + dd
+        }
+        if (mm < 10) {
+            mm = '0' + mm
+        }
 
-        const fechaEnvio = yyyy+'-'+mm+'-'+dd;
-        let impuestosDetalle =[];
-        if ( subtotalDoce > 0 ) {
+        const fechaEnvio = yyyy + '-' + mm + '-' + dd;
+        let impuestosDetalle = [];
+        if (subtotalDoce > 0) {
             impuestosDetalle.push(calcularImpuestosDetalle(subtotalDoce, totalIva, '2', '0.00'));
         }
-        if ( subtotalCero > 0 ) {
+        if (subtotalCero > 0) {
             impuestosDetalle.push(calcularImpuestosDetalle(subtotalCero, '0.00', '0', '0.00'));
         }
-        if ( subtotalNoIva > 0 ) {
+        if (subtotalNoIva > 0) {
             impuestosDetalle.push(calcularImpuestosDetalle(subtotalNoIva, '0.00', '6', '0.00'));
         }
-        if ( subtotalExento > 0 ) {
+        if (subtotalExento > 0) {
             impuestosDetalle.push(calcularImpuestosDetalle(subtotalExento, '0.00', '7', '0.00'));
         }
         detallesFactura.map(detalle => {
             return detalle.descripcion = detalle.descripcion.replace(/(\r\n|\n|\r)/gm, " ");
         });
-        if ( !clienteSeleccionado ) {
+        if (!clienteSeleccionado) {
             dispatch(startMostrarError('Debe seleccionar un cliente.'));
             return;
-        } else if ( detallesFactura.length === 0 ) {
+        } else if (detallesFactura.length === 0) {
             dispatch(startMostrarError('No existen detalles a facturar.'));
             return;
-        } else if ( formaPago === '' ) {
+        } else if (formaPago === '') {
             dispatch(startMostrarError('Seleccione la forma de pago.'));
             setRemarcar(true);
             return;
@@ -217,7 +217,7 @@ export const FacturaScreen = ({history}) => {
             detalleValores: {
                 totalSinImpuestos: subtotalSinImpuestos.toFixed(2),
                 totalDescuento: '0.00',
-                totalIva,
+                totalIva: totalIva.toFixed(2),
                 importeTotal: valorTotal.toFixed(2)
             }
         }));
@@ -254,10 +254,10 @@ export const FacturaScreen = ({history}) => {
             </div>
 
             <div className="flex flex-col flex-wrap md:flex-row mb-8 justify-between px-4 py-2 border-gray-200 border-l-2 border-b-2 border-r-2 rounded-md rounded-t-none text-left">
-                <div 
-                    className="w-10/12 md:w-5/12" 
+                <div
+                    className="w-10/12 md:w-5/12"
                 >
-                    <div 
+                    <div
                         className="mb-10"
                         ref={wrapperRef}
                     >
@@ -344,9 +344,9 @@ export const FacturaScreen = ({history}) => {
                         >Número Factura</label>
                         <p
                             className="w-full border-b-2 pb-1 border-gray-200 focus:outline-none focus:border-indigo-300 mt-2 text-sm"
-                        >{empresa?
-                                    `${empresa.establecimiento}-${empresa.puntoEmision}-${pad.substring(0, pad.length - empresa.secuencialFactura.length) + empresa.secuencialFactura}`
-                                    : null}</p>
+                        >{empresa ?
+                            `${empresa.establecimiento}-${empresa.puntoEmision}-${pad.substring(0, pad.length - empresa.secuencialFactura.length) + empresa.secuencialFactura}`
+                            : null}</p>
                     </div>
                     <div className="mb-10 flex justify-between flex-col">
                         <label
@@ -364,7 +364,7 @@ export const FacturaScreen = ({history}) => {
                             className="text-xs font-bold"
                         >Forma de Pago</label>
                         <select
-                            className={"w-full pb-1 mt-2 text-sm border-b-2 border-gray-200 focus:outline-none focus:border-indigo-300 focus:shadow-lg" + 
+                            className={"w-full pb-1 mt-2 text-sm border-b-2 border-gray-200 focus:outline-none focus:border-indigo-300 focus:shadow-lg" +
                                 ((remarcar && formaPago === '') ? ' border-2 border-red-400' : '')
                             }
                             name="formaPago"
@@ -372,15 +372,15 @@ export const FacturaScreen = ({history}) => {
                             onChange={handleInputChangeSelect}
                         >
                             <option value=''>--SELECCIONE--</option>
-                            { formasPago && formasPago.map(item => (
-                              <option key={item._id} value={item.codigo}>{item.formaPago}</option>
-                          )) }
+                            {formasPago && formasPago.map(item => (
+                                <option key={item._id} value={item.codigo}>{item.formaPago}</option>
+                            ))}
                         </select>
                     </div>
                 </div>
             </div>
             <div className="border-gray-200 border-2 rounded-md">
-                <Tabla 
+                <Tabla
                     titulo="Detalles Factura"
                     data={datosDetalles}
                     headers={headersDetalle}
@@ -415,17 +415,17 @@ export const FacturaScreen = ({history}) => {
                 </div>
             </div>
             <div className="flex justify-center">
-                <button 
+                <button
                     className="bg-blue-500 uppercase rounded-3xl py-3 px-8 text-white mr-4 focus:outline-none"
-                    onClick={handleEmitirFactura} 
+                    onClick={handleEmitirFactura}
                 >Enviar</button>
-                <NavLink 
+                <NavLink
                     className="bg-red-500 uppercase rounded-3xl py-3 px-8 text-white focus:outline-none"
                     to="/emitidas"
                 >Cancelar</NavLink>
             </div>
             {
-                mostrarNuevoDetalle && 
+                mostrarNuevoDetalle &&
                 <>
                     <NuevoDetalle
                         setShowModal={setMostrarNuevoDetalle}
@@ -433,7 +433,7 @@ export const FacturaScreen = ({history}) => {
                 </>
             }
             {
-                mostrarNuevoAdicional && 
+                mostrarNuevoAdicional &&
                 <>
                     <NuevoAdicional
                         setShowModal={setMostrarNuevoAdicional}
@@ -441,7 +441,7 @@ export const FacturaScreen = ({history}) => {
                 </>
             }
             {
-                mostrarNuevaFormaPago && 
+                mostrarNuevaFormaPago &&
                 <>
                     <NuevaFormaPago
                         setShowModal={setMostrarNuevaFormaPago}
@@ -453,7 +453,7 @@ export const FacturaScreen = ({history}) => {
                 mostrarCargando && <Cargando />
             }
             {
-                claveAcceso && <ImprimirComprobante claveAcceso={claveAcceso} history={history} tipoComprobante='factura'/>
+                claveAcceso && <ImprimirComprobante claveAcceso={claveAcceso} history={history} tipoComprobante='factura' />
             }
         </div>
     )
